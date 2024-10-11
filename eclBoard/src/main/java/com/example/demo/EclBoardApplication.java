8d88ee1e6bf554097cee70b80887a114d4783d38
rkdgusdnr99 <112935900+rkdgusdnr99@users.noreply.github.com>
2024-10-11 14:32:41 +0900

    git init

---
+++ eclBoard/src/main/java/com/example/demo/EclBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclBoardApplication.class, args);
+	}
+
+}
